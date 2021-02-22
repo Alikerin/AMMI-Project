f40d1a9966d9c85231d6bbe67053bfdcd7f185d3
Ibrahim Yusuf <i.yusuf@instadeep.com>
2021-02-22 09:40:26 +0100

    added gray option

--- create_spl_dataset.py
+++ create_spl_dataset.py
@@ -3,6 +3,8 @@ import glob
 import os
 import random
 
+import cv2
+import numpy as np
 from PIL import Image
 
 
@@ -12,6 +14,7 @@ def get_args():
     parser.add_argument("--new_dir", required=True, type=str)
     parser.add_argument("--n_repeat", required=True, type=int)
     parser.add_argument("--data_ratio", default=1.0, type=float)
+    parser.add_argument("--gray", default=False, action="store_true")
     return parser.parse_args()
 
 
@@ -27,16 +30,26 @@ def main(args):
         out_dir = os.path.join(new_dir, folder)
         os.makedirs(out_dir, exist_ok=True)
 
-        for img in folder_images:
+        for img in folder_images[:2]:
             img_filename = os.path.split(img)[-1].split(".")[0]
-            img = Image.open(img)
+            AB = Image.open(img)
+            w, h = AB.size
+            w2 = int(w / 2)
+            A = AB.crop((0, 0, w2, h))
+            B = AB.crop((w2, 0, w, h))
+            if args.gray:
+                A = cv2.cvtColor((np.asarray(B)), cv2.COLOR_RGB2GRAY)[:, :, np.newaxis]
+                A = Image.fromarray(np.tile(A, (1, 1, 3)))
             random_picks = random.choices(folder_images, k=args.n_repeat)
             for i, img2 in enumerate(random_picks):
                 new_img = Image.new("RGB", (256 * 3, 256))
                 img2 = Image.open(img2)
-                img2 = img2.crop((256, 0, 512, 256))
-                new_img.paste(img, (0, 0))
-                new_img.paste(img2, (512, 0))
+                w, h = img2.size
+                w2 = int(w / 2)
+                C = img2.crop((w2, 0, w, h))
+                new_img.paste(A, (0, 0))
+                new_img.paste(B, (w2, 0))
+                new_img.paste(C, (w2 * 2, 0))
                 new_img.save(os.path.join(out_dir, f"{img_filename}_{i}.jpg"))
 
 
